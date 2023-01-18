c4c41877c0a2cc20b1b05a5322abe8080b255dd2
nshy-ucsd <122579697+nshy-ucsd@users.noreply.github.com>
2023-01-17 22:01:01 -0800

    Initial commit

---
+++ starter/RPS.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class RPS extends RPSAbstract {
+    // Messages for the game.
+    protected static final String GAME_NOT_IMPLEMENTED =
+            "Game not yet implemented.";
+
+    /**
+     * Construct a new instance of RPS with the given possible moves.
+     * @param moves all possible moves in the game.
+     */
+    public RPS(String[] moves) {
+        this.possibleMoves = moves;
+        this.playerMoves = new String[MAX_GAMES];
+        this.cpuMoves = new String[MAX_GAMES];
+    }
+
+
+    public int determineWinner(String playerMove, String cpuMove) {
+        // TODO
+        return 0;  // replace this when you implement the method
+    }
+
+    public static void main(String[] args) {
+        // If command line args are provided use those as the possible moves
+        String[] moves = new String[args.length];
+        if (args.length >= MIN_POSSIBLE_MOVES) {
+            for (int i = 0; i < args.length; i++) {
+                moves[i] = args[i];
+            }
+        } else {
+            moves = RPS.DEFAULT_MOVES;
+        }
+        // Create new game and scanner
+        RPS game = new RPS(moves);
+        Scanner in = new Scanner(System.in);
+
+        // While user does not input "q", play game
+        System.out.println(GAME_NOT_IMPLEMENTED);
+
+        // TODO: Insert the code to play the game. 
+        // See the writeup for an example of the game play.
+        // Hint: call the methods we/you have already written 
+        // to do most of the work! And don't forget Javadoc.
+
+        in.close();
+    }
+}
